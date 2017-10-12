dea887946102aded22b31a1ef3ff26a80e74ee36
seantang <405806408@qq.com>
2017-10-12 17:44:55 +0800

    添加pageResult和queryObject

---
+++ src/main/java/com/_520it/crm/domain/Petservicemenu.java
@@ -0,0 +1,15 @@
+package com._520it.crm.domain;
+
+public class Petservicemenu {
+
+    private Long id;
+    private String name;
+    private Integer minprice;
+    private Integer maxprice;
+    private Long unit;
+    private String remark;
+    private Integer isdiscount;
+    private String discount;
+    private Long parentId;
+
+}
