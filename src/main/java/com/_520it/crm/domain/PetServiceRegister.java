5f26d9a99d051a98b0eb59f676d24e70cfd72ee5
seantang <405806408@qq.com>
2017-10-12 18:46:56 +0800

    添加PetServiceRegister

---
+++ src/main/java/com/_520it/crm/domain/PetServiceRegister.java
@@ -0,0 +1,24 @@
+package com._520it.crm.domain;
+
+import java.util.Date;
+
+public class PetServiceRegister {
+
+    private Long id;
+    private String petName;
+    private String masterName;
+    private String tel;
+    private String itemFirst;
+    private String itemSecond;
+    private Date startTime;
+    private Date endTime;
+    private Integer petAge;
+    private Integer petGender;
+    private String selfGood;
+    private String remark;
+    private Integer times;
+    private Integer state;
+    private Integer payment;
+    private Integer sevPrice;
+
+}
