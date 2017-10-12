ba3dd2515b6e795c0bcd8e55997f8fd1f8641319
zhangzhaoxin <343997217@qq.com>
2017-10-12 22:19:34 +0800

    创建收银管理的基本页面

--- src/main/java/com/_520it/crm/web/controller/CashBillItemController.java
+++ src/main/java/com/_520it/crm/web/controller/CashBillItemController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CashBillItemController {
     @RequestMapping("")
     public String index(){
-
         return "cashbillitem";
 
     }
