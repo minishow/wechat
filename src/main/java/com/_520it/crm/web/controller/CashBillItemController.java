9abfd72678e0b6f9a8baadbc504d105b9642ce28
zhangzhaoxin <343997217@qq.com>
2017-10-12 20:36:44 +0800

    创建收银管理的Controller

---
+++ src/main/java/com/_520it/crm/web/controller/CashBillItemController.java
@@ -0,0 +1,15 @@
+package com._520it.crm.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cashbillitem")
+public class CashBillItemController {
+    @RequestMapping("")
+    public String index(){
+        return "cashbillitem";
+    }
+}
+
