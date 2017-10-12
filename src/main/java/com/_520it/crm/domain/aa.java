15b526fa3a212b51ffaa18427987a726a6606067
zm <zm@520it.com>
2017-10-12 16:42:23 +0800

    在domain增加aa类

--- src/main/java/com/_520it/crm/domain/aa.java
+++ src/main/java/com/_520it/crm/domain/aa.java
@@ -2,6 +2,6 @@ package com._520it.crm.domain;
 
 public class aa {
     public static void main(String[] args) {
-        System.out.println("大大大大");
+        System.out.println("大大大大啊啊啊");
     }
 }
