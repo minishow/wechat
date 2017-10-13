2d420ea6d5ad824d6e796034b10e6a059cdc9741
zm <zm@520it.com>
2017-10-13 09:07:18 +0800

    新增employee.js 修改/static//js/jquery多了/ 修改employee.jsp  <table id="employee_datagrid"></table>

--- src/main/java/com/_520it/crm/web/controller/EmployeeController.java
+++ src/main/java/com/_520it/crm/web/controller/EmployeeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EmployeeController {
 
-//    @RequestMapping("")
-//    public String index(){
-//        return "employee";
-//    }
+    @RequestMapping("")
+    public String index(){
+        return "employee";
+    }
 }
