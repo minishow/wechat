9959abc79828b099e01315b4170451ff33b6a8d4
zm <zm@520it.com>
2017-10-13 09:44:32 +0800

    新增EmployeeController, 修改employee.js 增加分页的代码操作,增加分页sql

--- src/main/webapp/static/js/views/employee.js
+++ src/main/webapp/static/js/views/employee.js
@@ -11,11 +11,9 @@ $(function () {
             {field:'gender',title:'性别',width:100},
             {field:'tel',title:'联系电话',width:100},
             {field:'entryTime',title:'入职时间',width:100},
-            {field:'resignationTime',title:'名称',width:100},
-            {field:'basePay',title:'价格',width:100,align:'right'},
-            {field:'idCard',title:'价格',width:100,align:'right'}
+            {field:'resignationTime',title:'离职时间',width:100},
+            {field:'basePay',title:'基本工资',width:100,align:'right'},
+            {field:'idCard',title:'身份证号码',width:100,align:'right'}
         ]]
-
-
     })
 });
