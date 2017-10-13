2f0947b43a1846ef6a01c1047f07920daac33e18
Manolin <Manolin_m@163.com>
2017-10-13 15:31:50 +0800

    修正:给producinfo表添加会员价格 memberprice,给producttype添加name字段,给pageResult添加泛型

--- src/main/java/com/_520it/crm/page/PageResult.java
+++ src/main/java/com/_520it/crm/page/PageResult.java
@@ -1,14 +1,13 @@
 package com._520it.crm.page;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Setter@Getter@AllArgsConstructor
 public class PageResult {
 	private Long total;
-	private List row=new ArrayList();
+	private List<?> rows;
 }
