2af5cd8463edd3e03c9b48d81f5c279f311c918b
seantang <405806408@qq.com>
2017-10-13 11:48:55 +0800

    删除错误文件

--- src/main/java/com/_520it/crm/web/controller/MemberinfoController.java
+++
@@ -1,9 +0,0 @@
-package com._520it.crm.web.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/memberinfo")
-public class MemberInfoController {
-}
