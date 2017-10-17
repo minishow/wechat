2dad59ec13df745aa3c981d73159865c24aa6233
seantang <405806408@qq.com>
2017-10-17 19:06:49 +0800

    Merge branch 'master' of https://git.oschina.net/SeanTang323/byiaacrm
    # Conflicts:
    #	src/main/resources/generatorConfig.xml

--- src/main/java/com/_520it/crm/util/stockObject.java
+++ src/main/java/com/_520it/crm/util/stockObject.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
  */
 @Setter
 @ToString
-public class StockObject {
+public class stockObject {
     private Long stockIncomeId;
     private Long specificId;
     private Long productInfoId;
